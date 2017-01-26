c901333d0e4c81aa56bad67eb16af7ca4da8daee
Nour Betar <nourdt@live.com>
2017-01-26 17:07:39 +0800

    removed old code, and update to rails 3.2

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -11,7 +11,7 @@ Krb::Application.configure do
 
   # Show full error reports and disable caching
   config.consider_all_requests_local       = true
-  config.action_view.debug_rjs             = true
+  
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send
@@ -22,9 +22,9 @@ Krb::Application.configure do
 
   # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
-  
+
   #require "ruby-debug"
-  
+
   config.action_mailer.default_url_options = {:host => "krb.localhost:3000"}
   config.action_mailer.smtp_settings = {
     :address => "smtp.sendgrid.net",
@@ -34,11 +34,10 @@ Krb::Application.configure do
     :user_name => "trivektor@gmail.com",
     :password => "mnbvcxz"
   }
-  
+
   config.after_initialize do
     Configuration.site_url = 'http://krb.localhost:3000/'
   end
 
-  
-end
 
+end
